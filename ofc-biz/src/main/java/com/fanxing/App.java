7758160dbbf6a6f7feb0224d07649500c5261d73
chehuizi <840739100@qq.com>
2024-06-10 23:56:35 +0800

    init

---
+++ ofc-biz/src/main/java/com/fanxing/App.java
@@ -0,0 +1,13 @@
+package com.fanxing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
