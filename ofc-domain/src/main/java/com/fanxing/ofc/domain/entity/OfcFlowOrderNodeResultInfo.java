26c71867869d57eddc54c73f4fb5797faee13955
chehuizi <840739100@qq.com>
2024-12-10 00:04:24 +0800

    OFC

---
+++ ofc-domain/src/main/java/com/fanxing/ofc/domain/entity/OfcFlowOrderNodeResultInfo.java
@@ -0,0 +1,11 @@
+package com.fanxing.ofc.domain.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Data
+public class OfcFlowOrderNodeResultInfo extends DomainEntityBase {
+}
---
+++ ofc-domain/src/main/java/com/fanxing/ofc/domain/entity/OmsOfcOrderDetail.java
@@ -0,0 +1,15 @@
+package com.fanxing.ofc.domain.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author chehuizi
+ * OMS OFC单据明细
+ */
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Data
+public class OmsOfcOrderDetail extends DomainEntityBase {
+}
